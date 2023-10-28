55dac80379f2463555b474aa0d6d42361a2f4b64
Varun Murlidhar <vmurlidhar@unc.edu>
2023-10-28 15:39:57 -0400

    test

--- my-pill-tracker/src/pages/Calendar.js
+++ my-pill-tracker/src/pages/Calendar.js
@@ -1,20 +1,3 @@
-// import {useState} from 'react';
-// import { DayPilotCalendar } from "@daypilot/daypilot-lite-react";
-
-// const Calendar = () => {
-//     const [config, setConfig] = useState({
-//       viewType: "Week"
-//     });
-  
-//     return (
-//         <div>
-//             <DayPilotCalendar {...config} />
-//         </div>
-//     );
-//   }
-
-// export default Calendar;
-
 import React, { useState, useRef, useEffect } from 'react';
 import { DayPilot, DayPilotCalendar, DayPilotNavigator } from "@daypilot/daypilot-lite-react";
 // import "./CalendarStyles.css";
